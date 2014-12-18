7e6e395418d0bdd65dcabeac3c7f0e354784277f
Jack Rubio <jackrubio26@gmail.com>
2014-12-18 18:50:29 +0000

    grid initiated

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,17 @@
 class Grid
 
-  
+def initialize
+  @grid
+end
+
+
+def populate_board
+  letters = ("A".."J").to_a
+  numbers = (1..10).to_a
+  board = Hash.new
+  coord = letters.map {|x| numbers.map {|y| board["#{x}#{y}"] = Cell.new}}
+  coord
+end
 
 
 
