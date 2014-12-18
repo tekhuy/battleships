6ce57bd96cad4b8b49c0008cb5d59cbe2a9cf915
Jack Rubio <jackrubio26@gmail.com>
2014-12-18 18:25:43 +0000

    starting grid fresh

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,9 @@
 class Grid
 
-  def populate_board
-    letters = ("A".."J").to_a
-    numbers = (1..10).to_a
-    coords = Hash.new
-    coords = letters.map {|letter| numbers.map {|number| puts "#{letter}#{number}"}}
-  end
+  
+
+
 
 end
+
+
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,13 +1,11 @@
-# require './lib/grid'
+require './lib/grid'
 
-# describe Grid  do 
+describe Grid  do 
 
-#   let(:grid) {Grid.new}
-#   let(:cell) {double :cell}
+  let(:grid) {Grid.new}
+  let(:cell) {double :cell}
 
-#   it 'should contain 100 cells' do
-#     allow(cell.status).to be true
-#     expect(grid.populate_board).to eq 100
-#   end
-  
-# end
+  it '' do
+  end
+
+end
